414c1bbbfbe5993a3e62bbc06e4c9803321f8903
Robert Johansson <robertjo79@gmail.com>
2016-06-15 09:09:46 +0200

    Added partial i18n support for the non-error strings.

--- lib/simple_nested_comments/engine.rb
+++ lib/simple_nested_comments/engine.rb
@@ -1,5 +1,10 @@
 module SimpleNestedComments
   class Engine < ::Rails::Engine
     #isolate_namespace SimpleNestedComments -- Removed this so we can call the helper methods
+
+    # To get localization to load, per: http://stackoverflow.com/questions/23986512/app-cannot-access-translation-files-in-engine-or-gem
+    config.before_initialize do
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    end
   end
 end
